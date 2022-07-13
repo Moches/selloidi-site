21ace48fb3acddced80f28b1d62f83388a0c262b
moches <ndelaflor@gmail.com>
2022-07-13 14:22:46 -0500

    Sello Idi Project Initialization

---
+++ public/_js/app.js
@@ -0,0 +1,5 @@
+// JavaScript by Moches (c) 2022
+console.log('Sitio informativo en Certificación Sello IDI');
+console.log('Con acceso a herramienta de auto-diagnóstico.');
+console.log('Inclusión, Diversidad e Igualdad.');
+console.log('(c) 2022 - Sello Idi');
