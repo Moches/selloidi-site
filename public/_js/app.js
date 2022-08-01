0bb5111892814d347014164355fdc54c23493d47
moches <ndelaflor@gmail.com>
2022-08-01 15:06:00 -0500

    Started Responsive Menu Added JScript

--- public/_js/app.js
+++ public/_js/app.js
@@ -3,3 +3,19 @@ console.log('Sitio informativo en Certificación Sello IDI');
 console.log('Con acceso a herramienta de auto-diagnóstico.');
 console.log('Inclusión, Diversidad e Igualdad.');
 console.log('(c) 2022 - Sello Idi');
+
+const navSlide = () => {
+  const toggle = document.querySelector('.toggle');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  console.log(toggle);
+  console.log(nav);
+  console.log(navLinks);
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('.nav-active');
+  });
+};
+
+navSlide();
