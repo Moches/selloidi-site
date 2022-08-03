233759a4a422e130f3530534108ebe0584395f65
moches <ndelaflor@gmail.com>
2022-08-03 14:00:49 -0500

    Added FAQs JS fucntionality and CSS

--- public/_js/app.js
+++ public/_js/app.js
@@ -4,14 +4,16 @@ console.log('Con acceso a herramienta de auto-diagnóstico.');
 console.log('Inclusión, Diversidad e Igualdad.');
 console.log('(c) 2022 - Sello Idi');
 
+// Interaction Start
+
 const navSlide = () => {
   const toggle = document.querySelector('.toggle');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
 
-  console.log(toggle);
-  console.log(nav);
-  console.log(navLinks);
+  // console.log(toggle);
+  // console.log(nav);
+  // console.log(navLinks);
 
   toggle.addEventListener('click', () => {
     nav.classList.toggle('.nav-active');
@@ -19,3 +21,22 @@ const navSlide = () => {
 };
 
 navSlide();
+
+// Accordion for FAQs
+
+// var accordion = document.getElementsByClassName('accordion');
+// var i;
+
+// console.log(accordion);
+
+// for (i = 0; i<acc.length; i++) {
+//   accordion[i].addEventListener('click', function () {
+//     this.classList.toggle('active');
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === 'block') {
+//       panel.style.display = 'none';
+//     } else {
+//       panel.style.display = 'block';
+//     }
+//   });
+// }
