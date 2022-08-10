7eccc71f5899cfb9663ca58650faea479a25ca0e
moches <ndelaflor@gmail.com>
2022-08-10 17:24:54 -0500

    Added BackToTop and JS all pages

--- public/_js/app.js
+++ public/_js/app.js
@@ -1,26 +1,26 @@
-// JavaScript by Moches (c) 2022
-console.log('Sitio informativo en Certificación Sello IDI');
-console.log('Con acceso a herramienta de auto-diagnóstico.');
-console.log('Inclusión, Diversidad e Igualdad.');
-console.log('(c) 2022 - Sello Idi');
+// // JavaScript by Moches (c) 2022
+// console.log('Sitio informativo en Certificación Sello IDI');
+// console.log('Con acceso a herramienta de auto-diagnóstico.');
+// console.log('Inclusión, Diversidad e Igualdad.');
+// console.log('(c) 2022 - Sello Idi');
 
-// Interaction Start
+// // Interaction Start
 
-const navSlide = () => {
-  const toggle = document.querySelector('.toggle');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//   const toggle = document.querySelector('.toggle');
+//   const nav = document.querySelector('.nav-links');
+//   const navLinks = document.querySelectorAll('.nav-links li');
 
-  // console.log(toggle);
-  // console.log(nav);
-  // console.log(navLinks);
+//   // console.log(toggle);
+//   // console.log(nav);
+//   // console.log(navLinks);
 
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('.nav-active');
-  });
-};
+//   toggle.addEventListener('click', () => {
+//     nav.classList.toggle('.nav-active');
+//   });
+// };
 
-navSlide();
+// navSlide();
 
 // Accordion for FAQs
 
@@ -40,3 +40,36 @@ navSlide();
 //     }
 //   });
 // }
+
+// BACK TO TOP PROTOTYPE
+var backToTop = function () {
+  this.constructor();
+};
+
+backToTop.prototype = {
+  constructor: function () {
+    this.trigger = document.querySelector('button.back-to-top');
+
+    window.addEventListener(
+      'scroll',
+      function () {
+        if (window.scrollY > 900) this.trigger.classList.add('active');
+        else this.trigger.classList.remove('active');
+      }.bind(this)
+    );
+
+    this.trigger.addEventListener('click', function () {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    });
+  },
+};
+
+// START ON DOM AND CONTENT LOAD
+document.addEventListener('DOMContentLoaded', function () {
+  // ADD BACK TO TOP INSTANCE
+  new backToTop();
+});
